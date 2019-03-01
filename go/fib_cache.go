57a53fc7d02f11a2499b5d65abeec34d902dfc2a
ninlgde <ninlgde.ma@gmail.com>
2019-03-01 11:37:27 +0800

    update script

--- go/fib_cache.go
+++ go/fib_cache.go
@@ -10,7 +10,9 @@ func fib_recursion_cache(n int) float64 {
 	if cache[n] != 0 {
 		return cache[n]
 	}
-	cache[n] = fib_recursion_cache(n-1) + fib_recursion_cache(n-2)
+	// 这里先计算了f(n-2)，是因为有些语言有最大堆栈限制，先计算n-2可以使深度提高一倍
+	// 甚至可以略微提高效率
+	cache[n] = fib_recursion_cache(n-2) + fib_recursion_cache(n-1)
 	return cache[n]
 }
 
--- go/fib_iterator.go
+++ go/fib_iterator.go
@@ -3,7 +3,7 @@ package fibonacci
 func fib_iterator(n int) float64 {
 	a, b := 1.0, 1.0
 	for ; n > 1; n-- {
-		a, b = a+b, a
+		a, b = a+b, a // => t=a;a=a+b;b=t
 	}
 	return a
 }
